3b84226cdddf5f8db1dd1a0fda79633171d413d5
Wyattkriebel <95396763+Wyattkriebel@users.noreply.github.com>
2022-11-01 23:43:14 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+
+# the file name
+fileName = ('path_of_excel_file') # TODO: add a file input
+
+# store the new data into this variable called file Data
+fileData = pd.read_excel(fileName)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ titer.py
@@ -0,0 +1,10 @@
+
+
+class Titer:
+    def calculate_titer(self, hot_sample, cold_sample):
+        # TODO: Pass samples and calculate titer
+        pass
+
+    def label_titers(self, titer):
+        # TODO: for each sample calculated, label it with its corresponding name
+        pass
